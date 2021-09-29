559a8f287920a8fdfba77b0d4500a41fbb188c5e
hawk <linyanhou@qq.com>
2021-09-29 16:53:05 +0800

    games202
    games202

---
+++ demo/Games202/homework1/src/lights/DirectionalLight.js
@@ -0,0 +1,35 @@
+class DirectionalLight {
+
+    constructor(lightIntensity, lightColor, lightPos, focalPoint, lightUp, hasShadowMap, gl) {
+        this.mesh = Mesh.cube(setTransform(0, 0, 0, 0.2, 0.2, 0.2, 0));
+        this.mat = new EmissiveMaterial(lightIntensity, lightColor);
+        this.lightPos = lightPos;
+        this.focalPoint = focalPoint;
+        this.lightUp = lightUp
+
+        this.hasShadowMap = hasShadowMap;
+        this.fbo = new FBO(gl);
+        if (!this.fbo) {
+            console.log("无法设置帧缓冲区对象");
+            return;
+        }
+    }
+
+    CalcLightMVP(translate, scale) {
+        let lightMVP = mat4.create();
+        let modelMatrix = mat4.create();
+        let viewMatrix = mat4.create();
+        let projectionMatrix = mat4.create();
+
+        // Model transform
+
+        // View transform
+    
+        // Projection transform
+
+        mat4.multiply(lightMVP, projectionMatrix, viewMatrix);
+        mat4.multiply(lightMVP, lightMVP, modelMatrix);
+
+        return lightMVP;
+    }
+}
