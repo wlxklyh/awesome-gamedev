c2e9363c23b34088cfa462aefc092ab2623433f9
hawk <linyanhou@qq.com>
2021-11-08 22:54:30 +0800

    cpp test fwrite fread

---
+++ demo/CPPDemo/src/5_fwritefread/main.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cstdio>
+#include <string>
+#
+using namespace std;
+//描述：
+int main() {
+    {
+        FILE* file = fopen("test.txt","wb");
+        char strInput[]  = "asdf";
+        std::size_t size =  strlen(strInput);
+        fwrite(&size,sizeof(std::size_t),1,file);
+        fwrite(strInput,1,sizeof(strInput),file);
+        fclose(file);
+    }
+
+
+    {
+        FILE* file = fopen("test.txt","rb");
+        std::size_t size = 0;
+        fread(&size,sizeof(std::size_t),1,file);
+        char *strOutput  = new char[size];
+        fread(strOutput,1,size,file);
+        fclose(file);
+
+        std::cout<<strOutput<<endl;
+    }
+
+    return 0;
+}
