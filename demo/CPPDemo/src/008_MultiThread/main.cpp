4b655a44bceade667b031ee49d98bcad0b3f1f18
hawk <linyanhou@qq.com>
2022-03-22 17:12:37 +0800

    modify folder name
    modify folder name

---
+++ demo/CPPDemo/src/008_MultiThread/main.cpp
@@ -0,0 +1,277 @@
+﻿#include <iostream>
+#include <vector>
+#include <condition_variable>
+#include <mutex>
+#include <thread>
+#include <string>
+
+
+//生产者消费者模型：
+//有3个面包师傅，每个师傅每10秒钟生产一个面包，并将面包放入到一个可以盛20个面包的篮子里，有5个吃面包的消费者，每每10秒钟从篮子里拿出一个面包吃掉，请设计并实现这个模型
+
+std::chrono::milliseconds  BakeInternalTime(10000);//制作面包的间隔时间：10秒
+std::chrono::milliseconds  EatInternalTime(10000); //吃面包的间隔时间：10秒
+int BakerThreadNum = 3;      //3个面包师傅
+int CustomerThreadNum = 5;   //5个顾客
+
+int EachBakeNum = 15; //每个师傅生产面包的数量
+int EachEatNum = 3;  //每个顾客要吃多少个面包
+
+
+enum PersonType
+{
+    None,
+    Consumer,
+    Producer,
+};
+
+
+//多线程操作这个篮子 和 判断篮子是否满了 是否空了 和 篮子的流水日志
+class BreadBasket
+{
+public:
+    //面包篮子最多放20个面包
+    static const int Capacity = 20;
+    BreadBasket() { NowBreakNum = 0; }
+    int NowBreakNum;
+    //面包篮子是否为空 这个函数调用要在BreakNumMutex锁里面
+    bool IsEmpty()
+    {
+        return 0 == NowBreakNum;
+    }
+    //面包篮子是否满了 这个函数调用要在BreakNumMutex锁里面
+    bool IsFull()
+    {
+        return Capacity == NowBreakNum;
+    }
+
+    std::mutex BreakNumMutex;			//面包数量的锁
+    std::mutex ConsumerTakeOutMutex;	//消费者拿面包的互斥量 相当于消费者排队 后面的消费者排队的时候也可以玩手机做其他事
+    std::mutex BakerPutInMutex;			//面包师烘焙面包的互斥量 相当于面包师傅制造好面包也要排队放进篮子  排队时候面包其他面包师傅可以做面包 除非做完面包才要排队
+
+    std::condition_variable BasketNotFull;  //等待篮子有空位
+    std::condition_variable BasketNotEmpty; //等待篮子有面包
+
+    std::vector<std::string> VecBasketLog;
+    //BreakNumMutex也锁了这个日志 所以不会有多线程写入 和 cout
+    void AddBasketLog(std::string str)
+    {
+        VecBasketLog.push_back(str);
+        std::cout << str;
+    }
+};
+
+class Person
+{
+public:
+    Person(int id, BreadBasket* basket) :ID(id), Basket(basket) {}
+    int ID;
+    BreadBasket* Basket;
+    PersonType PersonType;
+};
+
+
+
+class Customer :Person
+{
+public:
+    Customer(int id, BreadBasket* basket) :Person(id, basket)
+    {
+        PersonType = Consumer;
+    }
+    std::string GetName()
+    {
+        return "顾客" + std::to_string(ID) + "号" + "(现在面包数量：" + std::to_string(Basket->NowBreakNum) + ")";
+    }
+    void EatBreak()
+    {
+        if (Basket == NULL)
+        {
+            //Error
+            return;
+        }
+        //1、顾客吃面包要竞争拿面包篮子的位置  这个顾客先到抢到这个位置 获取这个锁 占据了这个位置
+        std::unique_lock<std::mutex> lock(Basket->ConsumerTakeOutMutex);
+        //2、这个顾客要查看面包数量 所以要锁下（别给面包师傅放面包了  放面包会导致数量不准确）
+        std::unique_lock<std::mutex> lockBreakNum(Basket->BreakNumMutex);
+        //3.数量是否为空
+        while (Basket->IsEmpty())
+        {
+            //篮子是空的 然后这个顾客把篮子放回去 顾客等待有面包师傅放入面包  其他顾客被ConsumerTakeOutMutex阻塞着 所以这个顾客会优先
+            Basket->AddBasketLog(GetName() + "查看篮子空 等待面包师傅放入面包\n");
+            Basket->BasketNotEmpty.wait(lockBreakNum);
+        }
+        //4、篮子不为空 拿掉一个面包
+        Basket->NowBreakNum--;
+        //5、写日志（日志被lockBreakNum保护）
+        Basket->AddBasketLog(GetName() + "吃了面包\n");
+        //面包操作完成把篮子还回去
+        lockBreakNum.unlock();
+        //顾客还完篮子 离开拿面包篮子的位置
+        lock.unlock();
+    }
+};
+
+class Baker :Person
+{
+public:
+    Baker(int id, BreadBasket* basket) :Person(id, basket)
+    {
+        PersonType = Producer;
+    }
+    std::string GetName()
+    {
+        return "面包师傅" + std::to_string(ID) + "号" + "(现在面包数量：" + std::to_string(Basket->NowBreakNum) + ")";
+    }
+
+    void BakeBreak()
+    {
+        if (Basket == NULL)
+        {
+            //Error
+            return;
+        }
+        //制造面包的过程可以在锁外面做好 做好要放进篮子才锁这个bakerMutex
+        //制作面包
+
+        //1、锁了之后其他师傅要等这个师傅
+        std::unique_lock<std::mutex> lockPutIn(Basket->BakerPutInMutex);
+
+        //如果只用BreakNumMutex这个锁会导致 篮子满的时候 面包师傅来查看了下篮子满了  BakerPutInMutex作用之一是让最先查看篮子满的师傅等待 让给顾客去吃 顾客吃完 这个面包师傅先把面包放下
+
+        std::unique_lock<std::mutex> lockBreakNum(Basket->BreakNumMutex);//锁了之后顾客就不能去拿面包先
+
+        while (Basket->IsFull())
+        {
+            //日志受lockBreakNum 保护
+            Basket->AddBasketLog(GetName() + "查看篮子满了 等待顾客来买面包\n");
+            Basket->BasketNotFull.wait(lockBreakNum);//面包数量满了 放不进去了 让出去给消费者 后面的面包师傅都被BakerPutInMutex卡着 不会去抢占BreakNumMutex锁
+        }
+        //篮子没满 师傅放入面包
+        Basket->NowBreakNum++;
+        //日志受lockBreakNum 保护
+        Basket->AddBasketLog(GetName() + "放入了面包\n");
+        //告诉等待吃面包的顾客 篮子有面包了
+        Basket->BasketNotEmpty.notify_all();
+        //面包师傅已经把面包放入篮子 篮子还出去
+        lockBreakNum.unlock();
+        //面包师傅已经把面包放入篮子 拿篮子的位置也让出去
+        lockPutIn.unlock();
+    }
+};
+
+
+
+void BakerThread(int ID, BreadBasket* Basket)
+{
+    Baker baker(ID, Basket);
+    for (int i = 1; i <= EachBakeNum; ++i)
+    {
+        std::this_thread::sleep_for(BakeInternalTime);
+        baker.BakeBreak();
+    }
+}
+
+void CustomerThread(int ID, BreadBasket* Basket)
+{
+    Customer customer(ID, Basket);
+
+    for (int i = 1; i <= EachEatNum; ++i)
+    {
+        std::this_thread::sleep_for(EatInternalTime);
+        customer.EatBreak();
+    }
+}
+
+
+int main()
+{
+    BreadBasket* Basket = new BreadBasket();
+
+    std::vector<std::thread> BakerThreads;
+    std::vector<std::thread> CustomerThreads;
+
+    for (int i = 0; i < BakerThreadNum; i++)
+    {
+        BakerThreads.push_back(std::thread(BakerThread, i, Basket));
+    }
+
+    for (int i = 0; i < CustomerThreadNum; i++)
+    {
+        CustomerThreads.push_back(std::thread(CustomerThread, i, Basket));
+    }
+
+    for (int i = 0; i < BakerThreadNum; i++)
+    {
+        BakerThreads[i].join();
+    }
+    for (int i = 0; i < CustomerThreadNum; i++)
+    {
+        CustomerThreads[i].join();
+    }
+}
+
+
+//例子： 10毫秒的例子
+//面包师傅0号(现在面包数量：1)放入了面包
+//顾客1号(现在面包数量：0)吃了面包
+//面包师傅2号(现在面包数量：1)放入了面包
+//顾客0号(现在面包数量：0)吃了面包
+//面包师傅1号(现在面包数量：1)放入了面包
+//顾客4号(现在面包数量：0)吃了面包
+//顾客2号(现在面包数量：0)查看篮子空 等待面包师傅放入面包
+//面包师傅0号(现在面包数量：1)放入了面包
+//顾客2号(现在面包数量：0)吃了面包
+//顾客3号(现在面包数量：0)查看篮子空 等待面包师傅放入面包
+//面包师傅2号(现在面包数量：1)放入了面包
+//顾客3号(现在面包数量：0)吃了面包
+//顾客1号(现在面包数量：0)查看篮子空 等待面包师傅放入面包
+//面包师傅1号(现在面包数量：1)放入了面包
+//顾客1号(现在面包数量：0)吃了面包
+//顾客0号(现在面包数量：0)查看篮子空 等待面包师傅放入面包
+//面包师傅0号(现在面包数量：1)放入了面包
+//顾客0号(现在面包数量：0)吃了面包
+//顾客4号(现在面包数量：0)查看篮子空 等待面包师傅放入面包
+//面包师傅2号(现在面包数量：1)放入了面包
+//顾客4号(现在面包数量：0)吃了面包
+//顾客2号(现在面包数量：0)查看篮子空 等待面包师傅放入面包
+//面包师傅1号(现在面包数量：1)放入了面包
+//顾客2号(现在面包数量：0)吃了面包
+//顾客3号(现在面包数量：0)查看篮子空 等待面包师傅放入面包
+//面包师傅0号(现在面包数量：1)放入了面包
+//顾客3号(现在面包数量：0)吃了面包
+//顾客1号(现在面包数量：0)查看篮子空 等待面包师傅放入面包
+//面包师傅2号(现在面包数量：1)放入了面包
+//顾客1号(现在面包数量：0)吃了面包
+//顾客0号(现在面包数量：0)查看篮子空 等待面包师傅放入面包
+//面包师傅1号(现在面包数量：1)放入了面包
+//顾客0号(现在面包数量：0)吃了面包
+//顾客4号(现在面包数量：0)查看篮子空 等待面包师傅放入面包
+//面包师傅0号(现在面包数量：1)放入了面包
+//顾客4号(现在面包数量：0)吃了面包
+//顾客2号(现在面包数量：0)查看篮子空 等待面包师傅放入面包
+//面包师傅2号(现在面包数量：1)放入了面包
+//顾客2号(现在面包数量：0)吃了面包
+//面包师傅1号(现在面包数量：1)放入了面包
+//顾客3号(现在面包数量：0)吃了面包
+//面包师傅0号(现在面包数量：1)放入了面包
+//面包师傅2号(现在面包数量：2)放入了面包
+//面包师傅1号(现在面包数量：3)放入了面包
+//面包师傅0号(现在面包数量：4)放入了面包
+//面包师傅2号(现在面包数量：5)放入了面包
+//面包师傅1号(现在面包数量：6)放入了面包
+//面包师傅0号(现在面包数量：7)放入了面包
+//面包师傅2号(现在面包数量：8)放入了面包
+//面包师傅1号(现在面包数量：9)放入了面包
+//面包师傅0号(现在面包数量：10)放入了面包
+//面包师傅2号(现在面包数量：11)放入了面包
+//面包师傅1号(现在面包数量：12)放入了面包
+//面包师傅0号(现在面包数量：13)放入了面包
+//面包师傅2号(现在面包数量：14)放入了面包
+//面包师傅1号(现在面包数量：15)放入了面包
+//面包师傅0号(现在面包数量：16)放入了面包
+//面包师傅2号(现在面包数量：17)放入了面包
+//面包师傅1号(现在面包数量：18)放入了面包
+//面包师傅0号(现在面包数量：19)放入了面包
+//面包师傅2号(现在面包数量：20)放入了面包
+//面包师傅1号(现在面包数量：20)查看篮子满了 等待顾客来买面包
---
+++ demo/CPPDemo/src/020_CPP20_coroutine/main.cpp
@@ -0,0 +1,70 @@
+﻿/*
+ * co_vs_callback.cpp
+ *
+ *  Created on: Mar 13, 2019
+ *      Author: frank
+ */
+#include <iostream>
+#include <thread>
+#include <experimental/coroutine>
+#include <chrono>
+#include <functional>
+
+// clang++ -std=c++2a -fcoroutines-ts -lstdc++ co_vs_callback.cpp
+
+using call_back = std::function<void(int)>;
+void Add100ByCallback(int init, call_back f) // 异步调用
+{
+    std::thread t([init, f]() {
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+        f(init + 100);
+    });
+    t.detach();
+}
+
+struct Add100AWaitable
+{
+    Add100AWaitable(int init):init_(init) {}
+    bool await_ready() const { return false; }
+    int await_resume() { return result_; }
+    void await_suspend(std::experimental::coroutine_handle<> handle)
+    {
+        auto f = [handle, this](int value) mutable {
+            result_ = value;
+            handle.resume();
+        };
+        Add100ByCallback(init_, f); // 调用原来的异步调用
+    }
+    int init_;
+    int result_;
+};
+
+struct Task
+{
+    struct promise_type {
+        auto get_return_object() { return Task{}; }
+        auto initial_suspend() { return std::experimental::suspend_never{}; }
+        auto final_suspend() { return std::experimental::suspend_never{}; }
+        void unhandled_exception() { std::terminate(); }
+        void return_void() {}
+    };
+};
+
+Task Add100ByCoroutine(int init, call_back f)
+{
+    int ret = co_await Add100AWaitable(init);
+    ret = co_await Add100AWaitable(ret);
+    ret = co_await Add100AWaitable(ret);
+    f(ret);
+}
+
+int main()
+{
+    Add100ByCallback(5, [](int value){ std::cout<<"get result: "<<value<<"\n"; });
+    Add100ByCoroutine(10, [](int value){ std::cout<<"get result from coroutine1: "<<value<<"\n"; });
+    Add100ByCoroutine(20, [](int value){ std::cout<<"get result from coroutine2: "<<value<<"\n"; });
+    Add100ByCoroutine(30, [](int value){ std::cout<<"get result from coroutine3: "<<value<<"\n"; });
+    Add100ByCoroutine(40, [](int value){ std::cout<<"get result from coroutine4: "<<value<<"\n"; });
+    getchar();
+}
+
