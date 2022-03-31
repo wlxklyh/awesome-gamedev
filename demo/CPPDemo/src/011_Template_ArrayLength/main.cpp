d3681405b202f1047a90ebb64944100ee0704312
wlxklyh <linyanhou@qq.com>
2022-03-31 22:34:09 +0800

    获取数组长度

---
+++ demo/CPPDemo/src/011_Template_ArrayLength/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+template <typename Type, size_t N>
+inline size_t GetArrayLength(const Type(&)[N])
+{
+    return N;
+}
+
+int main()
+{
+    int a []= {1,1,1,1};
+    cout << GetArrayLength(a) << endl;
+}
