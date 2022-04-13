26fbedee3d46b8632605940464f18947557022be
wlxklyh <linyanhou@qq.com>
2022-04-13 12:39:58 +0800

    位运算

---
+++ demo/CPPDemo/src/030_Bit_Calculate/main.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <string>
+#include <bitset>
+
+using namespace std;
+
+//描述：
+int main() {
+    std::bitset<64> A(0xffffffffffff000f);
+    std::bitset<64> LeftBit(0xaaaaaaaaaaaaaaaa);
+    std::bitset<64> RightBit(0x5555555555555555);
+    
+    long long uint64_A = A.to_ullong();
+    long long uint64_LeftBit = LeftBit.to_ullong();
+    long long uint64_RightBit = RightBit.to_ullong();
+
+    long long uint64_A_Left = uint64_A & uint64_LeftBit;
+    long long uint64_A_Right = uint64_A & uint64_RightBit;
+
+    cout << std::bitset<64>(uint64_A_Left) << endl;
+    cout << std::bitset<64>(uint64_A_Right) << endl;
+
+    uint64_A = uint64_A_Left & (uint64_A_Right << 1);
+    cout << std::bitset<64>(uint64_A) << endl;
+
+    cout << "\n";
+    long long Left = (uint64_A << 31);
+    cout << std::bitset<64>(Left) << endl;
+    cout << "\n";
+    std::bitset<64> Result(uint64_A | Left);
+    cout << Result << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
