28adb9bd8c4ae0d5670d747758b561bc09975382
wlxklyh <linyanhou@qq.com>
2022-04-15 10:53:36 +0800

    cpp demo

--- demo/CPPDemo/src/007_STL/main.cpp
+++ demo/CPPDemo/src/007_STL/main.cpp
@@ -2,13 +2,15 @@
 #include <stdio.h>
 #include <map>
 #include <string>
+
 using namespace std;
+
 //描述：
 int main() {
     //std::map
-    std::map<std::string,std::string> propertiesMap;
-    std::cout<<propertiesMap["hello"]<<std::endl;
-    std::cout<<"hello"<<std::endl;
+    std::map<std::string, std::string> propertiesMap;
+    std::cout << propertiesMap["hello"] << std::endl;
+    std::cout << "hello" << std::endl;
 
 
     std::string str_dec = "aa2001, A Space Odyssey";
@@ -18,10 +20,10 @@ int main() {
 
     std::string::size_type sz;   // alias of size_t
 
-    int i_dec = std::stoi (str_dec,&sz);
-    int i_hex = std::stoi (str_hex,nullptr,16);
-    int i_bin = std::stoi (str_bin,nullptr,2);
-    int i_auto = std::stoi (str_auto,nullptr,0);
+    int i_dec = std::stoi(str_dec, &sz);
+    int i_hex = std::stoi(str_hex, nullptr, 16);
+    int i_bin = std::stoi(str_bin, nullptr, 2);
+    int i_auto = std::stoi(str_auto, nullptr, 0);
 
     std::cout << str_dec << ": " << i_dec << " and [" << str_dec.substr(sz) << "]\n";
     std::cout << str_hex << ": " << i_hex << '\n';
@@ -30,3 +32,6 @@ int main() {
 
     return 0;
 }
+
+
+
---
+++ demo/CPPDemo/src/007_STL_EMPTY_BACK/main.cpp
@@ -0,0 +1,49 @@
+﻿#include <vector>
+#include <string>
+#include <iostream>
+
+struct President {
+    std::string name;
+    std::string country;
+    int year;
+
+    President(std::string p_name, std::string p_country, int p_year)
+            : name(std::move(p_name)), country(std::move(p_country)), year(p_year) {
+        std::cout << "I am being constructed.\n";
+    }
+
+    President(const President &other)
+            : name(std::move(other.name)), country(std::move(other.country)), year(other.year) {
+        std::cout << "I am being copy constructed.\n";
+    }
+
+    President(President &&other)
+            : name(std::move(other.name)), country(std::move(other.country)), year(other.year) {
+        std::cout << "I am being moved.\n";
+    }
+
+    President &operator=(const President &other);
+};
+
+int main() {
+    President P("Franklin Delano Roosevelt", "the USA", 1936);
+    President &PRef = P;
+    std::vector<President> elections;
+    std::cout << "emplace_back:\n";
+    elections.emplace_back(PRef); //没有类的创建
+
+    std::vector<President> reElections;
+    std::cout << "\npush_back:\n";
+    reElections.push_back(PRef);
+
+    std::cout << "\nContents:\n";
+    for (President const &president: elections) {
+        std::cout << president.name << " was elected president of "
+                  << president.country << " in " << president.year << ".\n";
+    }
+    for (President const &president: reElections) {
+        std::cout << president.name << " was re-elected president of "
+                  << president.country << " in " << president.year << ".\n";
+    }
+
+}
