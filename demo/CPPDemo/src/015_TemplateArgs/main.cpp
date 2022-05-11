051f30e8d172f16214c0677eb6ea5be8398b2659
wlxklyh <linyanhou@qq.com>
2022-05-11 16:45:54 +0800

    模板参数数量
    模板参数数量

---
+++ demo/CPPDemo/src/015_TemplateArgs/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+template<typename... Ts>
+void magic(Ts... args)
+{
+
+    std::cout << sizeof...(args) << std::endl;
+}
+//描述：
+int main() {
+    magic(1,2,3,4);
+    return 0;
+}
