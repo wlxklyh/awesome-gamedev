695b2d32cbef5b8f24777f6a19fb682c96bfda4e
wlxklyh <linyanhou@qq.com>
2022-07-06 00:16:05 +0800

    sync

--- demo/CPPDemo/src/007_STL/main.cpp
+++ demo/CPPDemo/src/007_STL/main.cpp
@@ -1,12 +1,72 @@
 ﻿#include <iostream>
 #include <stdio.h>
 #include <map>
+#include <vector>
 #include <string>
 
 using namespace std;
 
+struct A{
+    int a;
+    A()
+    {
+        cout<<"默认构造函数"<<endl;
+    }
+
+    A(int tmpa):a(tmpa)
+    {
+        cout<<"带参数构造函数"<<endl;
+    }
+
+    A(int tmpa,int tmpb):a(tmpa)
+    {
+        cout<<"带参数构造函数(2个参数)"<<endl;
+    }
+
+    A(const A& c)
+    {
+        cout<<"拷贝构造函数"<<endl;
+    }
+
+    A(const A&& c)
+    {
+        cout<<"移动拷贝构造函数"<<endl;
+    }
+
+    A& operator=(const A& a)
+    {
+        cout<<"赋值构造函数"<<endl;
+        return *this;
+    }
+
+    A& operator=(const A&& a)
+    {
+        cout<<"移动赋值构造函数"<<endl;
+        return *this;
+    }
+};
+
 //描述：
 int main() {
+    //std::vector
+    vector<A> Avec0;
+    cout << "例子 vector" << endl;
+    cout << "Avec0.reserve(11);:" << endl;
+    Avec0.reserve(11);
+
+    cout << "Avec0.resize(10);:" << endl;
+    Avec0.resize(10);
+
+    cout << "Avec0.resize(11);:" << endl;
+    Avec0.resize(11);
+
+    cout << "Avec0.resize(9);:" << endl;
+    Avec0.resize(9);
+
+    cout << "Avec0.resize(11);:" << endl;
+    Avec0.resize(11);
+
+    return 0;
     //std::map
     std::map<std::string, std::string> propertiesMap;
     std::cout << propertiesMap["hello"] << std::endl;
