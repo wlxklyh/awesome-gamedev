a54061a71c74973f3ebdd7ef223dd66b438b778d
wlxklyh <linyanhou@qq.com>
2024-12-13 01:22:50 +0800

    协程

--- demo/Python/TestCoroutine/Main.py
+++ demo/Python/TestCoroutine/Main.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import asyncio
+import sys
 import time
 
 
@@ -27,47 +28,69 @@ async def frame_sleep(frame_count, result=None, *, loop=None):
 
 
 # 使用示例
-async def example_task():
-    print(f"Task running {time.time()}")
-    await asyncio.sleep(1)
-    print(f"Task completed {time.time()}")
+async def example_task(str_flag):
+    while True:
+        print(str_flag)
+        await asyncio.sleep(0.0)
 
 
 class GameEngine:
     class MyAsyncScheduler:
         def __init__(self):
-            self.loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(self.loop)
+            self.loop = asyncio.events.new_event_loop()
+            asyncio.events.set_event_loop(self.loop)
+            asyncio.events._set_running_loop(self.loop)
+            self.loop.stop()
+
+            def custom_exception_handler(loop, context):
+                exc = context.get("exception")
+                if exc:
+                    print("MyAsyncScheduler exception")
+                    if exc:
+                        import traceback
+                        traceback.print_exception(type(exc), exc, exc.__traceback__, file=sys.stderr)
+
+            self.loop.set_exception_handler(custom_exception_handler)
 
         def run_coroutine(self, coro):
-            asyncio.ensure_future(coro, loop=self.loop)
-            self.run_once()
+            asyncio.create_task(coro)
+            handle = asyncio.get_running_loop()._ready[0]
+            handle._run()
 
         def run_once(self):
-            self.loop.call_soon(self.loop.stop)
-            self.loop.run_forever()
-            # self.loop._run_once()
-
-    def run_once(self):
-        self.scheduler.run_once()
+            asyncio.get_running_loop()._run_once()
 
     def init(self):
         self.frame_num = 0
         self.scheduler = GameEngine.MyAsyncScheduler()
-        self.scheduler.run_coroutine(example_task())
+
+    def run_coroutine(self, str_flag):
+        print("==run_coroutine begin")
+        self.scheduler.run_coroutine(example_task(str_flag))
+        print("==run_coroutine end")
 
     def tick(self):
         print(f"tick[{self.frame_num}]:")
         self.frame_num = self.frame_num + 1
-        self.run_once()
+        print("==tick begin")
+        self.scheduler.run_once()
+        print("==tick end")
 
 
 if __name__ == "__main__":
     engine = GameEngine()
     engine.init()
 
+    flag1 = True
+    flag2 = True
     while True:
+        if flag1:
+            engine.run_coroutine("111111111")
+            flag1 = False
         import time
 
         time.sleep(1.0 / 60.0)
         engine.tick()
+        if flag2:
+            engine.run_coroutine("222222222")
+            flag2 = False
